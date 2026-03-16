57181f55c76f948b53c05cddf426a45764b59a95
PatrykBaka <s33758@pjwstk.edu.pl>
2026-03-16 15:17:05 +0100

    Dodanie ifa z sprawdzeniem liczby

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -3,3 +3,12 @@ int a = 10;
 int b = 10;
 int c = a + b;
 Console.WriteLine(c);
+
+if (c % 2 == 0)
+{
+    Console.WriteLine("Liczba parzysta!");
+}
+else
+{
+    Console.WriteLine("Liczba nieparzysta!");
+}
